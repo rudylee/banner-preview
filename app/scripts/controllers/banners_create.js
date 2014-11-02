4ab6a8f75f314fc841aa273029912b75f666a1fb
Rudy Lee <rudylee1988@gmail.com>
2014-11-03 09:58:32 +1100

    Showing the progress for file upload Create unique bucket for each file upload

--- app/scripts/controllers/banners_create.js
+++ app/scripts/controllers/banners_create.js
@@ -1,4 +1,5 @@
 /* global Firebase */
+/* jshint loopfunc: true */
 'use strict';
 
 /**
@@ -9,25 +10,31 @@
  * Controller of the bannerPreviewApp
  */
 angular.module('bannerPreviewApp')
-  .controller('BannersCreateCtrl', function ($scope, $firebase, $upload, configuration, $location) {
+  .controller('BannersCreateCtrl', function ($scope, $firebase, $upload, configuration) {
     $scope.selectedFiles = [];
     $scope.progress = [];
+    $scope.banner = {
+      id:  0,
+      name: ''
+    };
 
     var ref = new Firebase(configuration.firebaseUrl);
+    var sync = $firebase(ref);
 
     $scope.onFileSelect = function($files) {
-      $scope.selectedFiles = $files;
+      // Save banner before uploading file
+      save();
 
       for (var i = 0; i < $files.length; i++) {
-        var index = i;
-        var file = $files[index];
+        var file = $files[i];
+        var index = $scope.selectedFiles.push(file) - 1;
 
         $upload.upload({
           url: configuration.AWS.url,
           method: 'POST',
           data : {
-            key: file.name, 
-            AWSAccessKeyId: configuration.AWS.AccessKeyId ,
+            key: $scope.banner.id + '/' + file.name, 
+            AWSAccessKeyId: configuration.AWS.AccessKeyId,
             acl: 'private', 
             policy: configuration.AWS.policy,
             signature: configuration.AWS.signature,
@@ -37,15 +44,36 @@ angular.module('bannerPreviewApp')
           file: file,
         }).progress(function(event) {
           $scope.progress[index] = parseInt(100.0 * event.loaded / event.total);
+        }).success(function() {
+          addFile(file.name);
         });
       }
     };
 
-    $scope.save = function(banner) {
-      ref.push({
-        name: banner.name 
-      });
+    // Saves the banner to Firebase
+    function save() {
+      if($scope.banner.id !== 0) {
+        return;
+      }
 
-      $location.path('/');
-    };
+      if($scope.banner.name === '') {
+        $scope.banner.name = 'Untitled';
+      }
+
+      sync.$push({
+        name: $scope.banner.name,
+      }).then(function(ref) {
+        $scope.banner.id = ref.name();
+      });
+    }
+
+    // Adds new file to Firebase
+    function addFile(filename) {
+      var bannerRef = new Firebase(configuration.firebaseUrl + '/' + $scope.banner.id);
+      var bannerSync = $firebase(bannerRef);
+
+      bannerSync.$push('files', {
+        filename: filename 
+      });
+    }
   });
